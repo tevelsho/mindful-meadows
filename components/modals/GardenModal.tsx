bd2cc543e247f081f3e76cf2a4accb581a1f5af6
Jing Shun <ooijs234@gmail.com>
2025-03-14 15:42:20 +0800

    feat: add content for garden modal

--- components/modals/GardenModal.tsx
+++ components/modals/GardenModal.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { motion, AnimatePresence, Variants } from "framer-motion";
 
 export default function GardenModal({
@@ -13,7 +13,33 @@ export default function GardenModal({
   children?: React.ReactNode;
   id: number;
 }) {
+  let [userName, setUserName] = useState("");
+  let [userEmotion, setUserEmotion] = useState([]);
+  let [userSummary, setUserSummary] = useState("");
+
   useEffect(() => {
+    fetch("dataset/users.json")
+      .then((res) => res.json())
+      .then(
+        (
+          json: {
+            id: number;
+            name: string;
+            emotions: [];
+            summary: string;
+          }[]
+        ) => {
+          json.forEach((user) => {
+            if (user.id === id) {
+              setUserName(user.name);
+              setUserEmotion(user.emotions);
+              setUserSummary(user.summary);
+              console.log(user.name);
+            }
+          });
+        }
+      );
+
     if (isOpen) {
       document.body.style.overflow = "hidden"; // Prevent scrolling when modal is open
     } else {
@@ -69,17 +95,33 @@ export default function GardenModal({
             animate="animate"
             exit="exit"
             variants={modalVariants}
-            className="relative w-[90%] md:w-3/5 h-4/5 p-4 md:p-6 bg-gray-100 rounded-2xl shadow-xl border-2 border-border overflow-scroll"
+            className="relative w-[90%] md:w-4/5 h-1/2 p-4 md:p-12 bg-gray-100 rounded-2xl shadow-xl border-[1px] border-border overflow-scroll border-black font-sans flex flex-col"
             onClick={(e) => e.stopPropagation()} // Prevent closing when clicking inside the modal
           >
-            {id}
-            <button
-              onClick={onClose}
-              className="absolute top-6 right-8 text-secondary hover:text-tertiary"
-            >
+            <button onClick={onClose} className="absolute top-6 right-8">
               ✕
             </button>
-            {children}
+            <div className="flex flex-row">
+              <div className="w-7/12 flex-grow">
+                <h2 className="font-bold text-3xl">{userName}</h2>
+                <h3 className="pb-12">Choir's CCA Community Garden</h3>
+                <h3 className="underline text-xl pb-4">{`${userName} is feeling ${userEmotion}`}</h3>
+                <div>
+                  <p className="text-lg">{userSummary}</p>
+                  <p className="text-right italic text-sm">
+                    AI Generated Summary <br /> User chose to share
+                  </p>
+                </div>
+              </div>
+              <div className="w-5/12 flex flex-col items-center justify-center">
+                flower placeholder
+              </div>
+            </div>
+            <div className="mt-auto">
+              <button className="px-8 py-4 bg-blue-500 text-white rounded-lg">
+                This button to be neobrutalised
+              </button>
+            </div>
           </motion.div>
         </motion.div>
       )}
